d82dcee7957e2620becc0e7b83c6174a6401a378
LAPTOP-S058R7DU\LENOVO <sajithjayasekara123@gmail.com>
2018-09-19 14:26:29 +0530

    complete application

--- synchronizer_token_pattern/index.js
+++ synchronizer_token_pattern/index.js
@@ -1,6 +1,7 @@
+'use strict'
 const express = require('express');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 var session = require('express-session');
 const csurf = require('csurf');
 const uuid = require('uuid/v4');
@@ -22,14 +23,14 @@ app.use('/', express.static(__dirname + '/public'));
 const username = 'user';
 const password = '12345';
 
-var generateSid = function(){
-    var sid = uuid();
-    return sid;
+var generateSecret = function(){
+    var secret = uuid();
+    return secret;
 }
 
 app.use(session({
     key: 'user_sid',
-    secret: generateSid(),
+    secret: generateSecret(),
     resave: false,
     saveUninitialized: false,
     // cookie: {
@@ -68,6 +69,7 @@ app.post('/login', csrfMiddleware, function(req, res){
         
         req.session.csrf_token = req.csrfToken();
         res.redirect('/form_page');
+        console.log(req.session.csrf_token);
     
     }
     else{
@@ -83,16 +85,29 @@ app.post('/csrf_token', function(req,res){
 });
 
 app.get('/form_page',function(req,res){
-    res.sendFile(__dirname + '/public/form_page.html');
+    if (req.session.username && req.cookies.user_sid) {
+        res.sendFile(__dirname + '/public/form_page.html');
+    }
+    else{
+        res.redirect('/');
+    }
+    
 });
 
 app.post('/form_page',function(req,res){
-    if(req.body._csrf == req.session.csrf_token){
-        res.send("successfull");
+    if (req.session.username && req.cookies.user_sid) {
+        if(req.body._csrf == req.session.csrf_token){
+            res.send("successfull");
+        }
+        else{
+        
+            res.send("cannot proceed");
+        }
     }
     else{
-        res.send("cannot proceed");
+        res.redirect('/');
     }
+   
 });
 
 app.listen(3000, function(err){
